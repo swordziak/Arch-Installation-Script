5f6a43d622b56a55bf2b95e0a47c656cfbf33d63
Swordziak <marwinter@protonmail.com>
2025-09-02 23:43:38 +0200

    Fix AUR support & add GUI Package Manager option

--- Scripts/Post/Aur.sh
+++ Scripts/Post/Aur.sh
@@ -7,14 +7,51 @@ read AUR
 
 case $AUR in
     1)
-        arch-chroot /mnt sudo pacman -S --needed git base-devel
-        arch-chroot /mnt git clone https://aur.archlinux.org/yay.git
-        arch-chroot /mnt cd yay
-        arch-chroot /mnt makepkg -si
-        arch-chroot /mnt cd ..
-        arch-chroot /mnt rm -rf yay
+        read -p "Enter your username: " USERNAME
+        arch-chroot /mnt su -c "git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -sri --noconfirm" - $USERNAME
         break
         ;;
+    2)
+        break
+        ;;
+    *)
+        echo "Wrong choice. Try again."
+        ;;
+esac
+
+echo "Do you want to Install GUI Package Manager?"
+echo "1) Yes"
+echo "2) No"
+read -p "Your choice: " GUI
+
+case $GUI in
+    1)
+        if arch-chroot /mnt pacman -Q gnome-shell >/dev/null 2>&1 || \
+           arch-chroot /mnt pacman -Q xfce4-session >/dev/null 2>&1 || \
+           arch-chroot /mnt pacman -Q budgie-desktop >/dev/null 2>&1 || \
+           arch-chroot /mnt pacman -Q cinnamon >/dev/null 2>&1 || \
+           arch-chroot /mnt pacman -Q cosmic-session >/dev/null 2>&1 || \
+           arch-chroot /mnt pacman -Q marco >/dev/null 2>&1 || \
+           arch-chroot /mnt pacman -Q pantheon-session >/dev/null 2>&1; then
+            
+            echo "Installing Pamac..."
+            arch-chroot /mnt su -c "git clone https://aur.archlinux.org/pamac-all.git && cd pamac-all && makepkg -sri --noconfirm" - $USERNAME
+
+        elif arch-chroot /mnt pacman -Q plasma >/dev/null 2>&1 || \
+             arch-chroot /mnt pacman -Q lxqt-session >/dev/null 2>&1 || \
+             arch-chroot /mnt pacman -Q cutefish-core >/dev/null 2>&1 || \
+             arch-chroot /mnt pacman -Q deepin-session >/dev/null 2>&1; then
+            
+            echo "Installing Octopi..."
+            arch-chroot /mnt pacman -S octopi --noconfirm
+
+        else
+            echo "Installing Pamac..."
+            arch-chroot /mnt su -c "git clone https://aur.archlinux.org/pamac-all.git && cd pamac-all && makepkg -sri --noconfirm" - $USERNAME
+        fi
+        break
+        ;;
+
     2)
         break
         ;;
