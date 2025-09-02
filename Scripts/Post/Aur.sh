77d787fceece10e4eec5cfe646ef9046ee7b2b87
Swordziak <marwinter@protonmail.com>
2025-09-02 23:50:02 +0200

    Optimize syntax

--- Scripts/Post/Aur.sh
+++ Scripts/Post/Aur.sh
@@ -27,24 +27,20 @@ read -p "Your choice: " GUI
 case $GUI in
     1)
         if arch-chroot /mnt pacman -Q gnome-shell >/dev/null 2>&1 || \
-           arch-chroot /mnt pacman -Q xfce4-session >/dev/null 2>&1 || \
-           arch-chroot /mnt pacman -Q budgie-desktop >/dev/null 2>&1 || \
-           arch-chroot /mnt pacman -Q cinnamon >/dev/null 2>&1 || \
-           arch-chroot /mnt pacman -Q cosmic-session >/dev/null 2>&1 || \
-           arch-chroot /mnt pacman -Q marco >/dev/null 2>&1 || \
-           arch-chroot /mnt pacman -Q pantheon-session >/dev/null 2>&1; then
-            
+            arch-chroot /mnt pacman -Q xfce4-session >/dev/null 2>&1 || \
+            arch-chroot /mnt pacman -Q budgie-desktop >/dev/null 2>&1 || \
+            arch-chroot /mnt pacman -Q cinnamon >/dev/null 2>&1 || \
+            arch-chroot /mnt pacman -Q cosmic-session >/dev/null 2>&1 || \
+            arch-chroot /mnt pacman -Q marco >/dev/null 2>&1 || \
+            arch-chroot /mnt pacman -Q pantheon-session >/dev/null 2>&1; then
             echo "Installing Pamac..."
             arch-chroot /mnt su -c "git clone https://aur.archlinux.org/pamac-all.git && cd pamac-all && makepkg -sri --noconfirm && cd .. && rm -rf yay" - $USERNAME
-
         elif arch-chroot /mnt pacman -Q plasma >/dev/null 2>&1 || \
-             arch-chroot /mnt pacman -Q lxqt-session >/dev/null 2>&1 || \
-             arch-chroot /mnt pacman -Q cutefish-core >/dev/null 2>&1 || \
-             arch-chroot /mnt pacman -Q deepin-session >/dev/null 2>&1; then
-            
+            arch-chroot /mnt pacman -Q lxqt-session >/dev/null 2>&1 || \
+            arch-chroot /mnt pacman -Q cutefish-core >/dev/null 2>&1 || \
+            arch-chroot /mnt pacman -Q deepin-session >/dev/null 2>&1; then
             echo "Installing Octopi..."
             arch-chroot /mnt pacman -S octopi --noconfirm
-
         else
             echo "Installing Pamac..."
             arch-chroot /mnt su -c "git clone https://aur.archlinux.org/pamac-all.git && cd pamac-all && makepkg -sri --noconfirm && cd .. && rm -rf yay" - $USERNAME
