059ce90125a2f119f232d710b07064f91e4a17c1
Swordziak <marwinter@protonmail.com>
2025-09-02 09:31:10 +0200

    Add Flatpak choose

--- Install.sh
+++ Install.sh
@@ -182,8 +182,21 @@ else
     echo "Wrong choice. Try again."
 fi
 arch-chroot /mnt mkinitcpio -P
-arch-chroot /mnt pacman -S flatpak
-arch-chroot /mnt flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+echo "Do you want install Flatpak?"
+echo "1) Yes"
+echo "2) No"
+read FLATPAK
+
+if [[ "$FLATPAK" == "1" ]]; then
+    echo "Instaling Flatpak."
+    arch-chroot /mnt pacman -S flatpak
+    arch-chroot /mnt flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+elif [[ "$FLATPAK" == "2" ]]; then
+    :
+else
+    echo "Wrong choice. Try again."
+fi
 
 # --- SERVICES
 arch-chroot /mnt systemctl enable NetworkManager
