a48d919affceb6d0cf2d2ad626e012b7550de629
Swordziak <marwinter@protonmail.com>
2025-09-02 18:24:32 +0200

    Revert "Split SystemPackages.sh"

--- Scripts/Base/SystemPackages.sh
+++ Scripts/Base/SystemPackages.sh
@@ -4,7 +4,25 @@ chmod +x ./SystemPackages/*
 
 curl -s "https://archlinux.org/mirrorlist/?country=DE&country=PL&country=CZ&country=UA&country=LT&country=SK&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 25 - > /etc/pacman.d/mirrorlist
 
-./SystemPackages/Ucode.sh
+echo "Pick your CPU brand:"
+echo "1) AMD"
+echo "2) Intel"
+read CPU_CHOICE
+
+case $CPU_CHOICE in
+    1)
+        echo "Picked AMD. Installing AMD microcode."
+        UCODE="amd-ucode"
+        ;;
+    2)
+        echo "Picked Intel. Installing Intel microcode."
+        UCODE="intel-ucode"
+        ;;
+    *)
+        echo "Wrong choice. Installing both for safety."
+        UCODE="amd-ucode intel-ucode"
+        ;;
+esac
 
 pacstrap -K /mnt base linux linux-firmware $UCODE btrfs-progs nano man-pages man-db dosfstools ntfs-3g git base-devel
 genfstab -U /mnt >> /mnt/etc/fstab
