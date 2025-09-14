bcb09fe07ce63cf438f12617af9adf025ad75d4a
Swordziak <marwinter@protonmail.com>
2025-09-14 21:18:06 +0200

    Change LANG variable to generic en_US.UTF-8 and KEYMAP to us

--- Scripts/Base/Locale.sh
+++ Scripts/Base/Locale.sh
@@ -3,8 +3,8 @@
 arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
 arch-chroot /mnt hwclock --systohc
 arch-chroot /mnt locale-gen
-arch-chroot /mnt localectl set-locale LANG=pl_PL.UTF-8
-arch-chroot /mnt echo 'KEYMAP=pl' > /etc/vconsole.conf
+arch-chroot /mnt localectl set-locale LANG=en_US.UTF-8
+arch-chroot /mnt echo 'KEYMAP=us' > /etc/vconsole.conf
 echo "Enter hostname:"
 read HOST
 arch-chroot /mnt echo $HOST > /etc/hostname
