87ecc27e9e7e4a5a30af06dea133184d086d7bbd
Swordziak <marwinter@protonmail.com>
2025-09-14 22:52:21 +0200

    Add Installation start question

--- Install.sh
+++ Install.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-chmod +x ./Scripts/*
-
-./Scripts/Base.sh
-./Scripts/User.sh
-./Scripts/Packages.sh
-./Scripts/Post.sh
-
-umount -R /mnt
-reboot
+echo "Do you want install Arch Linux?"
+echo "1) Yes"
+echo "2) No"
+read -p "Your choice: " CHOICE
+case $CHOICE in
+    1|y|Y)
+        chmod +x ./Scripts/*
+        ./Scripts/Base.sh
+        ./Scripts/User.sh
+        ./Scripts/Packages.sh
+        ./Scripts/Post.sh
+        umount -R /mnt
+        reboot
+        ;;
+    *)
+        exit
+        ;;
+esac
